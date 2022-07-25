d35aa488b43d9fcc780c3933750807c7e37feefd
lpy12123213 <98199090+lpy12123213@users.noreply.github.com>
2022-07-25 21:43:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ os.chdir(path)
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
         Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.44',
-    'cookie': '_uuid=58E9F87D-9DF9-86910-D972-6102E4810EE72166149infoc; buvid3=C7D600BB-47C0-BCC4-9065-E11C902B271566461infoc; b_nut=1649515067; buvid4=FFAC8272-235A-96BD-D6EE-551C4D84EF7166461-022040922-+hGGnoawvNgIhx/+uEHLiElOrSLpLtIWWxgdx51sLIVZ4T78x4hr4w%3D%3D; nostalgia_conf=-1; CURRENT_BLACKGAP=0; buvid_fp_plain=undefined; blackside_state=0; rpdid=0zbfVGhipD|k9P7lIuA|1x1|3w1NDmsy; LIVE_BUVID=AUTO7716495683704445; fingerprint3=d4e5aed0143b24edfba50fc3ac142151; i-wanna-go-back=-1; CURRENT_QUALITY=80; fingerprint=c4e14d52aaa2f1a564f71c6b2d3d6c2d; SESSDATA=5afffe4d%2C1666185978%2C3c686%2A41; bili_jct=994a343f0f648982846ecb9ae5fd75a5; DedeUserID=450158456; DedeUserID__ckMd5=f1b89c51453a6646; sid=johfkykh; b_ut=5; buvid_fp=c4e14d52aaa2f1a564f71c6b2d3d6c2d; b_lsid=710E4C6F5_18051759290; bsource=search_bing; bp_video_offset_450158456=651968725507899400; PVID=1; innersign=0; CURRENT_FNVAL=80',
+    'cookie': 'Yours cookie',
 }
 # 变量提前定义
 
